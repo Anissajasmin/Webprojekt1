63f154eb6b79adaa27550ec07941f1161f43c860
nicoledeis <nd045@hdm-stuttgart.de>
2018-11-05 12:02:08 +0100

    neuneu

---
+++ Startseite.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Startseite Touch</title>
+    <link rel="stylesheet" type="text/css" href="Startseitecss.css">
+</head>
+
+
+
+
+<body>
+<div id="startseite">
+    <div id="header">
+        <h1 id="ueberschrift"> TOUCH </h1>
+
+    </div>
+    <div id="main">
+        <h2 id="unterueberschrift"> Connect with other people
+        </h2>
+        <a style="text-decoration:none;" href="Login_lesen.php">
+            <div id="buttonlogin">Login</div>
+        </a>
+
+        <a style="text-decoration: none;" href="Login_schreiben.php">
+            <div id="buttonregister">Register</div>
+        </a>
+
+    </div>
+    <div id="footer">
+    </div>
+
+</div>
+
+
+</body>
+</html>
