69d8a5835efdfc40c9dc231d9557dab3e91c887b
admin <Ab235@hdm-stuttgart.de>
2018-11-22 20:17:51 +0100

    Grundgerüst der Profilseite

---
+++ profilseite.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profilseite</title>
+    <link rel="stylesheet" type="text/css" href="profilseite.css">
+</head>
+<body>
+
+<div id ="profilseite">
+
+    <div id="header">
+        <h1>TOUCH</h1>
+        <label id="searcharea" for="search">Search</label>
+            <input type="search" id="search" placeholder="Friends, ..." >
+
+    </div>
+
+
+
+    <div id="main">
+
+    <div id="recommondation">
+        <h2 class="ueberschriftenmain">Recommondations</h2>
+    </div>
+
+     <div class="button8"> Touches</div>
+    </div>
+
+
+
+
+    <div id="background">
+        <div class="button6">Your Name</div>
+        <div class="button7">Your E-Mail</div>
+        <div class="button3">Friends</div>
+        <div class="button4">Posts</div>
+        <div class="button5">Touches</div>
+
+
+
+        <div class="button1">Save</div>
+        <div class="button2">Settings</div>
+        <div id="ueberschrift">
+            <h3> Your Profile</h3>
+        </div>
+
+
+            <form id= postbox2 action="Login_lesen.php" method="post">
+                     <textarea id="text" name="text" placeholder="Write something..." cols="40" rows="4"></textarea>
+                <br>
+                <input type="submit">
+            </form>
+
+
+
+    </div>
+
+
+
+
+
