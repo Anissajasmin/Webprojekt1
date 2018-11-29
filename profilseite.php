678a9eae4f0dd6e7f5f15980bafe252d1257c7aa
admin <Ab235@hdm-stuttgart.de>
2018-11-29 23:36:22 +0100

    Profilseite

--- profilseite.php
+++ profilseite.php
@@ -4,60 +4,66 @@
     <meta charset="UTF-8">
     <title>Profilseite</title>
     <link rel="stylesheet" type="text/css" href="profilseite.css">
+    <meta name = "viewport" content="width-device-width, initial-scale=1.0, maximum-scale=1.0, user scalelable=no">
 </head>
 <body>
 
-<div id ="profilseite">
 
-    <div id="header">
-        <h1>TOUCH</h1>
-        <label id="searcharea" for="search">Search</label>
-            <input type="search" id="search" placeholder="Friends, ..." >
+<div id="profilseite">
+
+    <div class id="header">
+        <h1>TOUCH </h1>
+
+        <div id = "searcharea" class ="header"><input placeholder= "Search here..." type="text" id="searchbox"/></div>
+        <div id = "notificationneu" class ="header"><img src="notificationneu.png"/><div>
+        <div id = "messageneu" class="header"><img src="messageneu.png"/><div>
+        <div id = "settingneu" class="header"><img src="settingneu.png"/><div>
 
     </div>
+
+     <div class="header1"
+        </div>
+      <div class="coverpad">
+      </div>
 
 
 
     <div id="main">
 
-    <div id="recommondation">
-        <h2 class="ueberschriftenmain">Recommondations</h2>
-    </div>
-
-     <div class="button8"> Touches</div>
-    </div>
-
-
-
-
-    <div id="background">
-        <div class="button6">Your Name</div>
-        <div class="button7">Your E-Mail</div>
-        <div class="button3">Friends</div>
-        <div class="button4">Posts</div>
-        <div class="button5">Touches</div>
-
-
-
-        <div class="button1">Save</div>
-        <div class="button2">Settings</div>
-        <div class="button9">Upload Picture</div>
-        <div id="ueberschrift">
-            <h3> Your Profile</h3>
+        <div id="recommondation">
+            <h2 class="ueberschriftenmain"> Recommondations
+            </h2>
         </div>
 
-
-            <form id= postbox2 action="Login_lesen.php" method="post">
-                     <textarea id="text" name="text" placeholder="Write something..." cols="40" rows="4"></textarea>
-                <br>
-                <input type="submit">
-            </form>
+        <div class="button8"> Touches</div>
+    </div>
+</div>
 
 
-
+<div id="background">
+    <div class="button1">Save</div>
+    <div class="button2">Settings</div>
+    <div class="button3">Friends</div>
+    <div class="button4">Posts</div>
+    <div class="button5">Touches</div>
+    <div class="button6">Your Name</div>
+    <div class="button7">Your E-Mail</div>
+    <div class="button9">Upload Picture</div>
+    <div class = "button10"></div>
+    <div id="ueberschrift">
+        <h3> Your Profile</h3>
     </div>
 
 
+    <form id=postbox2 action="login.php" method="post">
+        <textarea id="text" name="text" placeholder="Write something..." cols="40" rows="4">
+        </textarea>
+        <br>
+        <input type="submit">
+    </form>
 
 
 
+
+</body>
+</html>
