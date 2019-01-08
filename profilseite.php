58d1c3c03292a3ba6e73c0ffba9973c91752ceb8
nicoledeis <nd045@hdm-stuttgart.de>
2019-01-08 17:49:38 +0100

    Anpassung der Divs + Fehlerbehebung

--- profilseite.php
+++ profilseite.php
@@ -25,87 +25,139 @@
             </h2>
         </div>
 
-        <a style="..." href="">
-            <div class="button8">Touches</div>
-        </a>
+
 
         <div id="background">
+<a style="..." href="">
+    <div class="button9">
+    <div>
+        <?php
+        //Profilbild soll hier angezeigt werden
 
-            <a style="..." href="">
-                <div class="button9">
-                    <div>
+        $stmt = $pdo->prepare("SELECT * FROM profilbildlogin WHERE user_id = $user_id ");
 
-    <?php
-    //Profilbild soll hier angezeigt werden
+        $result = $stmt->execute();
+        while ($row = $stmt->fetch()) {
+            echo "<div id=\"button9\">";
+            echo "<img src='" . $row['profilbildtext'] . "'height='120' 'weight: 120'>";
+            echo "</div>";
+        }
 
-    $stmt = $pdo->prepare("SELECT * FROM profilbildlogin WHERE user_id = $user_id ");
+        ?>
+    </div>
+    </div>
+</a>
 
-    $result = $stmt->execute();
-    while ($row = $stmt->fetch()) {
-        echo "<div id=\"button9\">";
-        echo "<img src='" . $row['profilbildtext'] . "'height='120' 'weight: 120'>";
-        echo "</div>";
-    }
 
-    ?>
-                    </div>
-                </div>
-            </a>
 
-        <div id="tabellename">
-    <?php
-        echo $title['benutzername'];
-    ?>
-        </div>
+
+            <div id="tabellename">
+                <?php
+                echo $title['benutzername'];
+                ?>
+            </div>
 
             <div id="tabelleemail">
 
                 <?php
-                    echo $title['hdm_mail'];
+                echo $title['hdm_mail'];
                 ?>
 
             </div>
 
-        <a style="..." href="settings.php?user_id=<?php echo $user_id;?>">
-             <div class="button2">Settings</div>
-        </a>
+            <a style="" href="settings.php?user_id=<?php echo $user_id; ?>">
+                <?php
+                if ($my_id == $user_id) {
 
-        <div class="button3">Friends</div>
+                    ?>
+                    <div class="button2">Settings</div>
+                    <?php
+                }
+                ?>
+            </a>
 
-        <div class="button4">Posts</div>
+            <div class="button3">Friends</div>
+
+            <div class="button4">Posts</div>
 
             <br>
-        <hr class="strich">
+            <hr class="strich">
 
             <br>
-        <hr class="strich">
+            <hr class="strich">
 
-        <div class = "button10"></div>
-        <div id="ueberschrift">
-            <h3> Your Profile</h3>
-        </div>
+            <div class = "button10"></div>
+            <div id="ueberschrift">
+                <h3> Your Profile</h3>
+            </div>
+
+
+
+
+
 
 
         <?php
         //Posts des Nutzers der Profilseite anzeigen
-        $posts = $pdo->prepare("SELECT * FROM beitrag WHERE user_id= $user_id  ORDER BY zeitstempel DESC");
+        $posts = $pdo->prepare("SELECT * FROM beitrag WHERE user_id = :user_id AND posts IS NOT NULL OR user_id = :user_id AND bildtext IS NOT NULL ORDER BY zeitstempel DESC");
         $postsergebnis= $posts->execute(array(':user_id' => $user_id));
-            if ($postsergebnis) {
-                while ($row = $posts->fetch()) {
+        if ($postsergebnis) {
+        while ($row = $posts->fetch()) {
         ?>
-        <div id="postsdernutzer">
-            <small><?php echo $row['posts']?></small><br>
-            <small><img src=<?php echo $row['bildtext']?>height='200'></small><br>
-        </div>
+            <div id="postsdernutzer">
+                <small><?php echo $row['posts']?></small><br>
+                <small><?php echo "<img src='" .$row['bildtext']. "'height='200'>";?></small><br>
+            </div>
 
-        <?php
-                }
 
-            }
+            <?php
+        }
+
+        }
         ?>
 
-        </div>
+
+
+
+
     </div>
+
+
+
+        <div id="profile">
+            <h2 class="ueberschriftenmain"> Profile
+            </h2>
+
+            <div id="tabellename1">
+                <?php
+                echo $title['benutzername'];
+                ?>
+            </div>
+
+            <div id="tabelleemail1">
+                <?php
+                echo $title['hdm_mail'];
+                ?>
+
+            </div>
+
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
 
