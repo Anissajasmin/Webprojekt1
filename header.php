abb39e90a62623e691f48c01c417aab48062b702
nicoledeis <nd045@hdm-stuttgart.de>
2019-01-20 10:21:20 +0100

    kleine Änderung

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" type="text/css" href="header.css">
@@ -11,6 +12,7 @@ include_once "logincheck.php";
 include("datenbankpasswort.php");
 
 $my_id = $_SESSION['login-id'];
+$meine_id = $_SESSION['login-id'];
 $user_id = $_GET['user_id'];
 
 
@@ -19,8 +21,6 @@ $user_id = $_GET['user_id'];
 $visit_user = $pdo->prepare("SELECT * FROM login WHERE login_id=$user_id");
 $visit_user->execute();
 $title = $visit_user->fetch();
-
-
 
 
 ?>
@@ -48,91 +48,105 @@ $title = $visit_user->fetch();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-
-
 </head>
 
 
 <body>
-<div id="navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a id="fonttouch" class="navbar-brand">TOUCH</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a id="fonttouch" class="navbar-brand">TOUCH</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" id="fontfeed" href="hauptseite.php?user_id=<?php echo $my_id; ?>">Mein Feed
+                    <span
+                            class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                   data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    Profil
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" id="fontprofil" href="profilseite.php?user_id=<?php echo $my_id; ?>">Mein
+                        Profil</a>
+                    <a class="dropdown-item" id="fonteinstellungen"
+                       href="settings.php?user_id=<?php echo $my_id; ?>">Einstellungen</a>
+                </div>
+
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" id="fontfreunde" href="meinefreunde.php?user_id=<?php echo $my_id; ?>">Meine
+                    Freunde <span
+                            class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+        <div>
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" id="fontfeed" href="hauptseite.php?user_id=<?php echo $my_id; ?>">Mein Feed <span
-                                class="sr-only">(current)</span></a>
-                </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false">
-                        Profil
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" id="fontprofil" href="profilseite.php?user_id=<?php echo $my_id; ?>">Mein Profil</a>
-                        <a class="dropdown-item" id="fonteinstellungen" href="settings.php?user_id=<?php echo $my_id; ?>">Einstellungen</a>
-                    </div>
-
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" id="fontfreunde"href="meinefreunde.php?user_id=<?php echo $my_id; ?>">Meine Freunde <span
-                                class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-            <div>
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item dropdown">
-                        <a class="btn btn-outline-dark" style="font-family: 'Helvetica Neue'; font-size:120%; font-weight: 200;" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
-                           aria-expanded="false"> Notifications
-
-                            <?php
-                            include_once"notification.php"
-                            ?>
-
-            </div>
-
-
-            <?php
-            //Suchfunktion
-            if (isset($_POST["suche"])) {
-                $allebenutzername = $_POST["suche"];
-                $benutzersuche = $pdo->prepare("SELECT * FROM vlj_loginprofilbild WHERE benutzername = '$allebenutzername' AND aktiviert = 1");
-                if ($benutzersuche->execute()) {
-                    while ($row = $benutzersuche->fetch()) {
-                        $userid = $row ['login_id'];
-                        ?>
-                        <h3>
-                            <a href="profilseite.php?user_id=<?php echo $userid ?>"><img
-                                        src="<?php echo $row['profilbildtext'] ?>"></a>
-                            <a href="profilseite.php?user_id=<?php echo $userid ?>"><?php echo $row['benutzername'] ?></a>
-                        </h3>
+                    <a class="btn btn-outline-dark"
+                       style="font-family: 'Helvetica Neue'; font-size:120%; font-weight: 200;" href="#"
+                       id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                       aria-expanded="false"> Notifications
 
                         <?php
-                    }
-                } else {
-                    echo "<div>No user found</div>";
-                }
-            }
-            ?>
-
+                        include_once "notification.php"
+                        ?>
 
         </div>
 
         <form class="form-inline my-2 my-lg-0 ">
-            <input class="form-control mr-sm-2 " style="font-family: 'Helvetica Neue'; font-size:120%; font-weight: 200;"name="suche" type="search" placeholder="Suchen" aria-label="Search">
-            <button class="btn btn-outline-dark my-2 my-sm-0" style="font-family: 'Helvetica Neue'; font-size:120%; font-weight: 200;" id="suchebutton" type="submit">Suche</button>
+            <input class="form-control mr-sm-2 "
+                   style="font-family: 'Helvetica Neue'; font-size:120%; font-weight: 200;" name="suche"
+                   type="search" placeholder="Suchen" aria-label="Search">
+            <button class="btn btn-outline-dark my-2 my-sm-0"
+                    style="font-family: 'Helvetica Neue'; font-size:120%; font-weight: 200;" name="suche" id="suchebutton"
+                    type="submit">Suche
+            </button>
+
+
+        <?php
+        //Suchfunktion
+
+        if (isset($_POST["suche"])) {
+            $allebenutzername = $_POST["suche"];
+
+            $benutzersuche = $pdo->prepare("SELECT * FROM profilbildlogin WHERE benutzername ='$allebenutzername' AND aktiviert = 1");
+            if ($benutzersuche->execute()) {
+
+                while ($row = $benutzersuche->fetch()) {
+                    $userid = $row['login_id'];
+                    ?>
+                    <h3>
+                        <a class="bla" href="profilseite.php?user_id=<?php echo $userid ?>"><img src="<?php echo $row['profilbildtext'] ?>"></a>
+                        <a class="bla" href="profilseite.php?user_id=<?php echo $userid ?>"><?php echo $row['benutzername'] ?></a>
+                    </h3>
+
+                    <?php
+                }
+            } else {
+                echo "<div>No user found</div>";
+            }
+        }
+        ?>
         </form>
 
 
-        <button type="button"  class="btn btn-outline-dark " style=" font-family: 'Helvetica Neue'; color:white; font-size:120%; font-weight: 200; background-color:#7B0202;"" ><a style= color:white; href="logout.php">Log Out</a></button>
 
 
-    </nav>
+
+
+    <button type="button" class="btn btn-outline-dark "
+            style=" font-family: 'Helvetica Neue'; color:white; font-size:120%; font-weight: 200; background-color:#7B0202;""><a style=color:white; href="logout.php">Log Out</a></button>
+
+
+</nav>
 </div>
 
 </body>
