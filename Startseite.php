56b8ac02c669c6beba37a6bfc7ad3fa057d2909a
nicoledeis <nd045@hdm-stuttgart.de>
2019-01-22 16:53:11 +0100

    Startseite ist responsiv (Anissa)

--- Startseite.php
+++ Startseite.php
@@ -4,31 +4,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>Startseite Touch</title>
     <link rel="stylesheet" type="text/css" href="Startseite.css">
+    <link href="bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="bootstrap.min.js"></script>
+    <script src="jquery-3.2.1.slim.min.js"></script>
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+            integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+            crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 
-
-
 <body>
-<div id="startseite">
-    <div id="header">
-        <h1 id="ueberschrift"> TOUCH </h1>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-3">
+        </div>
 
-    </div>
-    <div id="main">
-        <h2 id="unterueberschrift"> Connect with other people
-        </h2>
-        <a style="text-decoration:none;" href="login.php">
-            <div id="buttonlogin">Login</div>
-        </a>
-
-        <a style="text-decoration: none;" href="registrieren.php">
-            <div id="buttonregister">Register</div>
-        </a>
-
-    </div>
-    <div id="footer">
+        <div class="col-xs-6"
+        <div id="header">
+            <h1 id="ueberschrift"> TOUCH </h1>
+        </div>
     </div>
 
+    <div class ="col-xs-3">
+    </div>
+
+</div>
+</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-3">
+        </div>
+
+        <div class="col-xs-6"
+        <div id="main">
+            <h1 id="unterueberschrift"> Connect with other people </h1>
+            <a style="text-decoration:none;" href="login.php">
+                <div id="buttonlogin">Login</div>
+
+                <a style="text-decoration: none;" href="registrieren.php">
+                    <div id="buttonregister">Registrieren</div>
+                </a>
+        </div>
+    </div>
+
+    <div class ="col-xs-3">
+    </div>
 </div>
 
 
